1a4e123f4737a5b4ac2069b9cf04d1adf612999e
Melbeck777 <molmin729@gmail.com>
2025-11-03 22:15:53 +0900

    [add] Added border line between windows

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,19 +1,23 @@
 -- %LOCALAPPDATA%/nvim/lua/plugins/tokyonight.lua
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		style = "storm", -- "storm" | "moon" | "night" | "day"
-		transparent = false,
-		terminal_colors = true,
-		styles = {
-			comments = { italic = true },
-			keywords = { italic = true },
-		},
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
-		vim.cmd.colorscheme("tokyonight")
-	end,
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        style = "night", -- "storm" | "moon" | "night" | "day"
+        on_highlights = function(h1, c)
+            h1.WinSeparator = { fg = "#3b4261", bg = c.bg }
+            h1.FloatBorder = { fg = "#3b4261", bg = c.bg_dark }
+        end,
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+            comments = { italic = true },
+            keywords = { italic = true },
+        },
+    },
+    config = function(_, opts)
+        require("tokyonight").setup(opts)
+        vim.cmd.colorscheme("tokyonight")
+    end,
 }
