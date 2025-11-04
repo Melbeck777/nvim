fbd1afa6a70cafd39b81966307e006a6d91690b3
Melbeck777 <molmin729@gmail.com>
2025-11-04 22:31:46 +0900

    [Add] Added mason-tool-installer

---
+++ lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,15 @@
+return {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    event = "VeryLazy",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+        require("mason").setup()
+        require("mason-tool-installer").setup({
+            -- ensure_installed = {
+            --     "pyright", "ruff", "black",
+            -- },
+            auto_update = true,
+            run_on_start = true,
+        })
+    end,
+}
