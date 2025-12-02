56da75f9c649497bf75a135c832699d00da16f92
Melbeck777 <molmin729@gmail.com>
2025-12-02 22:20:41 +0900

    [add] Added fine-cmdline to float cmdline

---
+++ lua/plugins/fine-cmdline.lua
@@ -0,0 +1,19 @@
+return {
+	"VonHeikemen/fine-cmdline.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	config = function()
+		local fine = require("fine-cmdline")
+
+		fine.setup({
+			popup = {
+				border = {
+					style = "rounded",
+					text = { top = " Cmdline ", top_align = "left" }, -- ここは自由。全角でもOK
+				},
+				win_options = {
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+				},
+			},
+		})
+	end,
+}
