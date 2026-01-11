5e56f8e1476ac214218a188b48e0af313b59673e
Melbeck777 <molmin729@gmail.com>
2026-01-12 08:01:48 +0900

    [fix] Fixed coron

--- lua/plugins/autolist.lua
+++ lua/plugins/autolist.lua
@@ -1,18 +1,18 @@
 -- lua/plugins/autolist.lua
 return {
-  "gaoDean/autolist.nvim",
-  ft = { "markdown", "text", "tex", "plaintex", "norg" },
-  opts = {
-    lists = {
-      marker = { "-", "*", "+" },
-      checkbox = { " ", "x" },
-      auto_detect = true,
-      cycle = true,
-      indent = 2,
-    },
-    colon = true,
-  },
-  config = function(_, opts)
-    require("autolist").setup(opts)
-  end,
+	"gaoDean/autolist.nvim",
+	ft = { "markdown", "text", "tex", "plaintex", "norg" },
+	opts = {
+		lists = {
+			marker = { "-", "*", "+" },
+			checkbox = { " ", "x" },
+			auto_detect = true,
+			cycle = true,
+			indent = 2,
+		},
+		colon = { ":", ";" },
+	},
+	config = function(_, opts)
+		require("autolist").setup(opts)
+	end,
 }
