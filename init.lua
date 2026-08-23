af39df49c0c754fa119d8220d4301fe8014b6073
Melbeck777 <molmin729@gmail.com>
2026-08-23 21:03:28 +0900

    add: encoding for read japanese

--- init.lua
+++ init.lua
@@ -73,6 +73,28 @@ opt.whichwrap = "b,s,h,l,<,>,[,]"
 -- クリップボード設定のスペルも修正（unnamedplus）
 opt.fileformats = { "dos", "unix", "mac" }
 
+-- 日本語ファイルの文字コード自動判別（Excel 由来の CSV は cp932 が多い）
+-- 前から順に試して、デコードに失敗しなかったものが採用される。
+-- euc-jp はバイト範囲が厳しく先に置いても cp932 を誤検出しにくいので、この順序にする。
+opt.fileencodings = {
+	"ucs-bom", -- BOM 付き UTF-8 / UTF-16（Excel の「UTF-8」出力）
+	"utf-8",
+	"iso-2022-jp",
+	"euc-jp",
+	"cp932", -- Shift_JIS
+	"latin1",
+}
+-- 自動判別が外れたときに開き直すコマンド: :Enc cp932 など
+vim.api.nvim_create_user_command("Enc", function(o)
+	vim.cmd("edit ++enc=" .. o.args)
+end, {
+	nargs = 1,
+	complete = function()
+		return { "utf-8", "cp932", "euc-jp", "iso-2022-jp", "utf-16le", "latin1" }
+	end,
+	desc = "指定した文字コードで開き直す",
+})
+
 opt.helplang = { "ja", "en" }
 
 opt.updatetime = 300
